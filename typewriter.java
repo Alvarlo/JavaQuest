e26e4d60623df78d00bdef73cb86fadd551dc12c
Alvarlo <alvaro.s401816@cesurformacion.com>
2024-11-08 10:23:59 +0100

    typewriter

---
+++ typewriter.java
@@ -0,0 +1,14 @@
+public class typewriter {
+    public static void main(String[] args) {
+        String text = "Bienvenido a JavaQuest";
+        int i;
+        for(i = 0; i < text.length(); i++){
+            System.out.printf("%c", text.charAt(i));
+            try{
+                Thread.sleep(100);//pause between characters
+            }catch(InterruptedException ex){
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
