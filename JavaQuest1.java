97dd58b143fb5aa48292a3490cd71a80d7840e44
Alvarlo <alvaro.s401816@cesurformacion.com>
2024-11-08 10:26:49 +0100

    num

--- JavaQuest1.java
+++ JavaQuest1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class JavaQuest1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
     }
 }
