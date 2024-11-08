70d2e55212d78c8a0cc8763fba0637613dcdb56d
Zluciffer <patrykwor0@gmail.com>
2024-11-08 11:50:37 +0100

    D&S

---
+++ DangeonAndStone.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class DangeonAndStone {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Muy buenas aventurero, bien venido a Dangeon and Stone");
+        System.out.println("En primer lugar has de escoger tu raza la caul se ajusta a una clase");
+        System.out.println("Las opciones son las siguientes:");
+        System.out.println("1º Enano, clase guerrero");
+        System.out.println("HP = ");
+        System.out.println("ATK = ");
+        System.out.println("DEF = ");
+        System.out.println("AGI = ");
+        System.out.println("SP = ");
+        System.out.println("\n" +
+                "MMWk. ,doxOOXMWKo:;;:oKWMMWOlldkOKWMMMMM\n" +
+                "MMNc  ..  ..lNNc     .oWMM0,     ,0MMMMM\n" +
+                "MMWo. ;:.,od0WNl      'kNN0:.     lWMMMM\n" +
+                "MMMXo.:c'kMMMMWo       .',;;.     '0MMMM\n" +
+                "MMMMWKO:;XMMMWx.                   lWMMM\n" +
+                "MMMMMMO,lWMMXo.                    .OMMM\n" +
+                "MMMMMNl.oWMWk.              ...     lNMM\n" +
+                "MMMMWx. .cxkl.            .dKXd.    'OMM\n" +
+                "MMMMWk..   ..             lNMMX;     lNM\n" +
+                "MMMMMNK0xc'.''           .xMMMWO;.   '0M\n" +
+                "MMMMMMMMMWX0Kk.          .OMMMMMWKx:.,0M\n" +
+                "MMMMMMMMMMMNk:            lXMMMMMMMWK0NM\n" +
+                "MMMMMMMMMWk;               cXMMMMMMMMMMM\n" +
+                "MMMMMMMWKl.                 lNMMMMMMMMMM\n" +
+                "MMMMMMNk'                   .xWMMMMMMMMM\n" +
+                "MMMMMMWx.                    .oNMMMMMMMM\n" +
+                "MMMMMM0'     ..  .oxo,        '0MMMMMMMM\n" +
+                "MMMMMMd     :OKkoOWWWO.      ;0WMMMMMMMM\n" +
+                "MMMMMWo    ;0MMMMMMMMNx.   .oXWMMMMMMMMM\n" +
+                "MMMMWO'  .dXWMMMMMMMMMWd.  ;KMMMMMMMMMMM\n" +
+                "MMWOc.   :NMMMMMMMMMMMMx.  .dXWMMMMMMMMM\n" +
+                "MMWkccllo0MMMMMMMMMMMMM0c,...:0MMMMMMMMM\n" +
+                "");
+        System.out.println("2º Elfo, clase arquero");
+        System.out.println("ATK = ");
+        System.out.println("DEF = ");
+        System.out.println("AGI = ");
+        System.out.println("SP = ");
+        System.out.println("               ...  ........=.......    \n" +
+                "              ........:......=....   .. \n" +
+                "     ......... ..:==:.........#..... .  \n" +
+                "     ....*@@*=..*@@@@.... ....:*.... .. \n" +
+                "     .....=@@@@@@@@@%:::.......#...     \n" +
+                "     .......#@@@@@@@%=:.....:=@%:-. . . \n" +
+                "     .....:#@@@@@@@@@@@@@@@%#***.... .. \n" +
+                "     ..#%....@@@@@@@+........:#...... . \n" +
+                "     ..+@#:..%@@@@@-........-*.....   . \n" +
+                "     ....=#%+@@@@@::=-....:=:........   \n" +
+                "..     ...#@@@@@@@%-...-=-..... .. .... \n" +
+                ".... ....:%@@@@@@@@-..+:.. ..       ....\n" +
+                "  ....:++@@@@@@@@@@@..:... ..          .\n" +
+                ".. .....*@@@@@@@@@@@+...            .   \n" +
+                ". .....:%@@@@@@@@@@@%..                .\n" +
+                "........@@@#*@@@@@@@@%...         .  ...\n" +
+                "..   ...@@-...::-#@@@@+..         ......\n" +
+                "  .....-@%.........*@@@-.         ......\n" +
+                "... ...*@.. ...... .%@*.      . ....... \n" +
+                "......:@@.      ....:@@..    . ...      \n" +
+                "....-%@**.    ..   ..=@+.    ... .      \n" +
+                "                   ...#@.....           \n" +
+                "                   ...-@%....           \n" +
+                "                   ....+%....           ");
+        System.out.println("3º Hombre bestia, Doble espada");
+        System.out.println("ATK = ");
+        System.out.println("DEF = ");
+        System.out.println("AGI = ");
+        System.out.println("SP = ");
+        System.out.println("  ...      .....    ....      ...   \n" +
+                "     ....     ....      ....:.  ...     \n" +
+                "              ..:.         .::          \n" +
+                "       ..   ..::....::...  ..:..        \n" +
+                "       ...  .--...+%%%%=.....:-.  ..    \n" +
+                "     .... ..=:...+@@@%*+:..  .-:.....   \n" +
+                "     ..   .+:. ..:**#+%*+:.. .:-. .     \n" +
+                "        ..+-. ..+%%%#%#++++-...=-       \n" +
+                "       ..+-....=+*##+*###**++*=:*:.     \n" +
+                "      .-===-**##%%*+=+*#:..:=*+*##+..   \n" +
+                "     ..=---:=++-.+#+*%*-...      :+.    \n" +
+                "       ..     ..+%#%##%*=..     ...     \n" +
+                "            ..:%%%#=+%#**=*...          \n" +
+                "            .:++*%+#+=+###+-:...        \n" +
+                "       ...  .###%%@%-=*@#**++-.. ....   \n" +
+                "     .... ..+=#+-**:...:+%**+*=......   \n" +
+                "     .. ...=+@#+*-..   ...+**++:.       \n" +
+                "       ...-#@*=%=.        .:#*=+.       \n" +
+                "       .=**##=*:...        .:##+.  ..   \n" +
+                "     .:=*%%#%=.....     ......===....   \n" +
+                "   ..=+-+=:.......      .......+%+--... \n" +
+                " ..:*%-...... .            ...-+##*=:.. \n" +
+                "..-*%*.....                    ......   \n" +
+                ".:=*##=-:..              ...            ");
+        System.out.println("4º Barbaro espadón");
+        System.out.println("ATK = ");
+        System.out.println("DEF = ");
+        System.out.println("AGI = ");
+        System.out.println("SP = ");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
+                "@@@@@@#:...::.+@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
+                "@@@@@@#@:.::::::@%=::*@@@@@@@@@@@@%@@@@@\n" +
+                "@@@@@@@@@#.::....::::::::.%@@@@@@*%#@@@@\n" +
+                "@@@@@@@@*.:....:::*%+::=%#:%@@@@++%@@@@@\n" +
+                "@@@@@@:........:**%%%-:%%:--@@@#==#@@@@@\n" +
+                "@@@@@+.........:*+....+=-**#@@@*++#@@@@@\n" +
+                "@@@@@........=#-*%%#=::::+#-@@@==*#%@@@@\n" +
+                "@@@@@*.:=====:=.*+::=#*#%:+:-=%==*##@@@@\n" +
+                "@@@@@+-=++*+*+-.::::::::::::-+-==*%@@@@@\n" +
+                "@@@@#-*--==+++=.:::::::::::-*#%%%#+#%@@@\n" +
+                "@@@@##%%%%%%#=+:.::::::::::::--+**+*@@@@\n" +
+                "@@@@+%#%%%%*:=**=+*-:....-=*::---#@@@@@@\n" +
+                "@@@@+%%%%%*%%%*%=**==-*++*=+-%#**@@@@@@@\n" +
+                "@@@@@*#%#%%%%%*#-:+:::--::.-+###=@@@@@@@\n" +
+                "@@@@@@@+**#%##*=+-*+++-:::..:::+%@@@@@@@\n" +
+                "@@@@@@@@=:::=***-*#++***#*=--:-+@@@@@@@@\n" +
+                "@@@@@@@@::.+#-:=+=-***=:#%+++#@@@@@@@@@@\n" +
+                "@@@@@@@@=-==+-=+@@@@@@:-+=+==%@@@@@@@@@@\n" +
+                "@@@@@@+::::-=++@@@@@@@+=+=::::-@@@@@@@@@\n" +
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("4º Barbaro espadón");
+        System.out.println("ATK = ");
+        System.out.println("DEF = ");
+        System.out.println("AGI = ");
+        System.out.println("SP = ");
+        System.out.println("");
+
+    }
+}
