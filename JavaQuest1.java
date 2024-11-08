824a5875b74a61c2f1a77a106ef4fab9f5df5729
Zluciffer <patrykwor0@gmail.com>
2024-11-08 10:56:04 +0100

    acciones y apartados

--- JavaQuest1.java
+++ JavaQuest1.java
@@ -5,5 +5,38 @@ public class JavaQuest1 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
 
+        
+
+        /*
+        1. Selección de acciones: En cada turno, tanto el jugador como el oponente pueden elegir una
+           acción para sus personajes (por ejemplo, atacar, defender, usar una habilidad especial o
+           cambiar de criatura).
+         */
+        /*
+        2. Resolución del turno: Una vez que se han seleccionado las acciones, el turno se resuelve. Las
+           acciones pueden tener diferentes efectos, como infligir daño al oponente, curar al propio
+           personaje o aplicar estados especiales como parálisis o quemaduras.
+         */
+        /*
+        3. Actualización de estados: Después de la resolución del turno, se actualizan los estados de
+           los personajes. Esto incluye la reducción de puntos de vida (HP) por daño sufrido, la
+           aplicación de efectos secundarios y otros cambios en las estadísticas.
+         */
+        /*
+        4. Turno del oponente: Luego, el oponente selecciona y ejecuta su acción. El proceso se repite
+           hasta que uno de los personajes quede sin HP y se declare al ganador.
+         */
+        /*
+        Apartado 1: Creación de personajes (2 puntos)
+        Apartado 2: Creación del enemigo (0.5 puntos)
+        Apartado 3: Implementación de la batalla (4 puntos)
+        Apartado 4: Finalización de la batalla (0.5 puntos)
+        Apartado 5: Interfaz de usuario (0.5 puntos)
+        Apartado 6: Generación de sprites (0.5 puntos)
+        Apartado 7: Enemigo inteligente (1 punto)
+                    Fase 1 (mientras tenga más de un 15% de vida)
+                    Fase 2 (mientras tenga menos de un 15% de vida)
+         */
+
     }
 }
