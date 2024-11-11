9d070f0c872b54cb78d0d9f06a3dd565e864f0b3
Alvarlo <alvaro.s401816@cesurformacion.com>
2024-11-11 14:26:02 +0100

    Alvaro11-11

--- JavaQuestAlvaro.java
+++ JavaQuestAlvaro.java
@@ -8,42 +8,48 @@ public class JavaQuestAlvaro {
         //Cada personaje tiene diferentes estadísticas, como puntos de vida, puntos de ataque y puntos de defensa.
         //Almacena estas estadísticas en variables.
 
-        String text1 = "Bienvenido a JavaQuest";
+
+        String text1 = "Bienvenido a JavaQuest"
+                +"\nSelecciona tu clase"
+                +"\n "
+                +"\n- Guerrero"
+                +"\n- Mago"
+                +"\n- Arquero" +
+                "\n";
         int i;
         for(i = 0; i < text1.length(); i++){
             System.out.printf("%c", text1.charAt(i));
             try{
-                Thread.sleep(100);//pause between characters
+                Thread.sleep(90);//pause between characters
             }catch(InterruptedException ex){
                 Thread.currentThread().interrupt();
             }
         }
 
-        System.out.println(" ");
-
-
-
-        String text2 = "Selecciona tu clase:";
-        int j;
-        for(j = 0; j < text2.length(); j++){
-            System.out.printf("%c", text2.charAt(j));
-            try{
-                Thread.sleep(100);//pause between characters
-            }catch(InterruptedException ex){
-                Thread.currentThread().interrupt();
-            }
-        }
-        System.out.println("");
-        System.out.println("- Guerrero");
-        System.out.println("- Mago");
-        System.out.println("- Arquero");
         Scanner scl = new Scanner(System.in);
 
         String seleccionClase = scl.nextLine();
 
         int vida = 0, ataque = 0, defensa = 0;
 
-        switch (seleccionClase){
+
+        if (seleccionClase.equalsIgnoreCase("Guerrero")) {
+            System.out.println("Has seleccionado Guerrero, tus estadísticas son: ");
+            vida = (int) (Math.random()*31+60);
+            ataque = (int) (Math.random()*16+20);
+            defensa = (int) (Math.random()*21+15);
+        }else if (seleccionClase.equalsIgnoreCase("Mago")) {
+            System.out.println("Has seleccionado Mago, tus estadísticas son: ");
+            vida = (int) (Math.random()*31+60);
+            ataque = (int) (Math.random()*16+20);
+            defensa = (int) (Math.random()*11+5);
+        }else if (seleccionClase.equalsIgnoreCase("Arquero")) {
+            System.out.println("Has seleccionado Arquero, tus estadísticas son: ");
+            vida = (int) (Math.random()*31+60);
+            ataque = (int) (Math.random()*16+20);
+            defensa = (int) (Math.random()*16+15);
+        }
+        /* switch (seleccionClase){
             case "Guerrero":
                 System.out.println("Has seleccionado Guerrero");
                 vida = (int) (Math.random()*31+60);
@@ -51,19 +57,20 @@ public class JavaQuestAlvaro {
                 defensa = (int) (Math.random()*21+50);
                 break;
             case "Mago":
-                System.out.println("Has seleccionado Mago");
-                vida = (int) (Math.random()*21+45);
-                ataque = (int) (Math.random()*21+35);
-                defensa = (int) (Math.random()*21+35);
+                System.out.println("Has seleccionado Guerrero");
+                vida = (int) (Math.random()*31+60);
+                ataque = (int) (Math.random()*16+20);
+                defensa = (int) (Math.random()*21+50);
                 break;
             case "Arquero":
-                System.out.println("Has seleccionado Arquero");
-                vida = (int) (Math.random()*21+50);
-                ataque = (int) (Math.random()*21+25);
-                defensa = (int) (Math.random()*21+40);
+                System.out.println("Has seleccionado Guerrero");
+                vida = (int) (Math.random()*31+60);
+                ataque = (int) (Math.random()*16+20);
+                defensa = (int) (Math.random()*21+50);
                 break;
+                }
 
-        }
+         */
         System.out.println("Vida = " + vida);
         System.out.println("Ataque = " + ataque);
         System.out.println("Defensa = " + defensa);
@@ -85,31 +92,31 @@ public class JavaQuestAlvaro {
                 //Goblin
                 vidaE = (int) (Math.random()*21+40);
                 ataqueE = (int) (Math.random()*21+10);
-                defensaE = (int) (Math.random()*21+20);
+                defensaE = (int) (Math.random()*16+10);
                 nombreE = "Goblin";
                 break;
             case 1:
                 //Bandido
                 vidaE = (int) (Math.random()*21+45);
                 ataqueE = (int) (Math.random()*21+15);
-                defensaE = (int) (Math.random()*21+15);
+                defensaE = (int) (Math.random()*16+5);
                 nombreE = "Bandido";
                 break;
             case 2:
                 //Slime
                 vidaE = (int) (Math.random()*21+50);
                 ataqueE = (int) (Math.random()*21+5);
-                defensaE = (int) (Math.random()*21+30);
+                defensaE = (int) (Math.random()*16+15);
                 nombreE = "Slime";
                 break;
         }
 
-        double PorcentajevidaE = (double) (vidaE * 15) /100; // esto es para el patrón de ataque cuando tiene menos de un 15% de vida
+        // double PorcentajevidaE = (double) (vidaE * 15) /100; // esto es para el patrón de ataque cuando tiene menos de un 15% de vida
 
         System.out.println("Te enfrentas a : " + nombreE);
-        System.out.println("VidaE = " + vidaE);
-        System.out.println("AtaqueE = " + ataqueE);
-        System.out.println("DefensaE = " + defensaE);
+        System.out.println("Vida " + nombreE + " = " + vidaE);
+        System.out.println("Ataque " + nombreE + "= " + ataqueE);
+        System.out.println("Defensa " + nombreE + " = " + defensaE);
 
 
         /*
@@ -120,14 +127,59 @@ public class JavaQuestAlvaro {
         Actualiza los puntos de vida de cada uno después de cada turno.
         */
 
+        String atacar = "atacar", defender = "defender", eleccion ="";
+        int turno = 1;
+        int vidaDefensa = vida;
+        while (vida > 0 && vidaE > 0 ){
 
 
 
+            System.out.println("Turno " + turno);
+            System.out.println("Qué quieres hacer: ");
+            System.out.println("- Atacar");
+            System.out.println("- Defender");
+            eleccion = scl.nextLine();
 
+            if (eleccion.equalsIgnoreCase(atacar)){
+                vidaE -= (ataque -(defensaE/2));
+                System.out.println("Has atacado, el enemigo tiene " + vidaE + " puntos de vida restantes");
+            } else if (eleccion.equalsIgnoreCase(defender)) {
+                vidaDefensa = vida + (defensa);
+                System.out.println("Te defiendes, aumentando tu vida a " + vidaDefensa + " durante este turno");
+            }else {
+                System.out.println("Respuesta no válida");
+                continue;
+            }
+
+            System.out.println("El enemigo te ataca, quitandote " + ataqueE + " puntos de vida");
+
+            if (eleccion.equalsIgnoreCase(atacar)){
+                System.out.println("Tu vida actual es  " + (vida-ataqueE));
+                vida -= ataqueE;
+            }else {
+                if ((vidaDefensa-ataqueE)>vida){
+                    System.out.println("Has bloqueado todo el ataque, tu vida actual es  " + vida);
+                }else {
+                    System.out.println("Tu vida actual es  " + (vidaDefensa-ataqueE));
+                    vida -= (vidaDefensa-ataqueE);
+                }
+            }
+            System.out.println("La vida del enemigo es " + vidaE);
+            System.out.println("Termina el turno:");
+
+            turno++;
+        }
+        
+        /*
+        Apartado 4: Finalización de la batalla (0.5 puntos)
+        */
+
+        if (vida > vidaE){
+            System.out.println("Has ganado!! Has obtenido un pin del fari con musiquita");
+        }else System.out.println("Vaya.... has perdido, hasta luego! ");
 
 
         /*
-        Apartado 4: Finalización de la batalla (0.5 puntos)
         Apartado 5: Interfaz de usuario (0.5 puntos)
         Apartado 6: Generación de sprites (0.5 puntos)
         Apartado 7: Enemigo inteligente (1 punto)
